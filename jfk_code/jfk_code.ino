77c706174f5c968d9ab016355e02b951853b62bc
Ella Gault <71401690+ellad@users.noreply.github.com>
2023-11-03 13:31:26 -0500

    Actual jfk_code.ino

--- jfk_code/jfk_code.ino
+++ jfk_code/jfk_code.ino
@@ -1,11 +1,48 @@
+#include <Servo.h>
+Servo bullet;
+Servo jfkHead;
+Servo dpd;
+const int gunButtonPin=2;
+const int cpOneBP=3;
+const int cpTwoBP=4;
+const int cpThreeBP=5;
+const int lhoBP=6;
+int shotCount=0;
 void setup() 
 {
-  // put your setup code here, to run once:
-//cheve
+  bullet.attach(9);
+  jfkHead.attach(10);
+  dpd.attach(11);
+  pinMode(gunButtonPin, INPUT);
+  pinModepinMode(cpOneBP, INPUT);
+  pinMode(cpTwoBP, INPUT);
+  pinMode(cpThreeBP, INPUT);
+  pinMode(lhoBP, INPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //cheve stinks
-  //no u stink (stinker and smeelll bad!(Cheve is insecure about his stench and is projecting))
+  if(digitalRead(lhoBP)==HIGH)
+  {
+    if(digitalRead(cpOneBP)==HIGH || digitalRead(cpTwoBP==HIGH))
+    {
+      if(gunButtonPin==HIGH)
+      {
+        bullet.write(180);
+        shotCount++;
+      }
+      else
+      {
+        bullet.write(0);
+      }
+    }
+    if(shotCount==3) 
+    {
+      jfkHead.write(90);
+    }
+    if(digitalRead(cpThreeBP==HIGH))
+    {
+      dpd.write(180);
+    }
+}
 }
