dcaa488966e8b7715974455d4233b60b9229b64f
Cheve G <149627382+milquetoastt@users.noreply.github.com>
2023-11-13 13:18:11 -0600

    Update jfk_code.ino

--- jfk_code/jfk_code.ino
+++ jfk_code/jfk_code.ino
@@ -1,10 +1,68 @@
+#include <Servo.h>
+Servo bullet;
+Servo jfkHead;
+Servo dpd;
+const int gunButtonPin=2;
+const int cpOneBP=3;
+const int cpTwoBP=4;
+const int cpThreeBP=5;
+const int lhoBP=6;
+int shotCount=0;
 void setup() 
 {
-  // put your setup code here, to run once:
-//chevewdawdawd
+  Serial.begin(9600);
+  bullet.attach(9);
+  jfkHead.attach(10);
+  dpd.attach(11);
+  pinMode(gunButtonPin, INPUT);
+  pinMode(cpOneBP, INPUT);
+  pinMode(cpTwoBP, INPUT);
+  pinMode(cpThreeBP, INPUT);
+  pinMode(lhoBP, INPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  if(digitalRead(cpOneBP)){
+    Serial.println("1");
+    bullet.write(180);
+  }
+  if(digitalRead(cpTwoBP)){
+    Serial.println("2");
+    bullet.write(0);
+  }
+  if(digitalRead(cpThreeBP)){
+    Serial.println("3");
+  }
+
+
+  if(digitalRead(lhoBP)
+  {
+    
+      if(shotCount<1&&digitalRead(gunButtonPin))
+      {
+        bullet.write(180);
+        shotCount++;
+        delay(300);
+        bullet.write(0);
+      }
+    
+     if(shotCount<3&&digitalRead(cpTwoBP))
+     {
+       bullet.write(180);
+       shotCount++;
+       delay(300);
+       bullet.write(0);
+     }
+    
+    if(shotCount==3) 
+    {
+      jfkHead.write(90);
+    }
+    if(digitalRead(cpThreeBP)
+    {
+      dpd.write(180);
+    }
+  }
 }
